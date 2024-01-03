65ddb829402a413807eb816771b9d6fd995be268
Nishit Bhardwaj <nishitjrt@gmail.com>
2024-01-03 19:49:43 +0530

    Text field added to modal

--- lib/widgets/modal.dart
+++ lib/widgets/modal.dart
@@ -10,18 +10,53 @@ class Modal extends StatefulWidget{
 }
 
 class _Modal extends State<Modal>{
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding( 
-      padding: EdgeInsets.all(20),
+    return Padding( 
+      padding: const EdgeInsets.all(20),
       child: Column( 
         children: [ 
-          TextField( 
+          TextField(
+            controller: _titleController, 
             maxLength: 25,
-            decoration: InputDecoration( 
+            decoration: const InputDecoration( 
               label: Text("Title")
             ),
-          )
+          ),
+          TextField( 
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration( 
+              label: Text("Amount spent")
+            ),
+          ),
+          Row( 
+            children: [ 
+              ElevatedButton(
+                onPressed: (){
+                  print(_titleController.text);
+                  print(_amountController.text);
+                }, 
+                child: const Text("Save expense") 
+                ),
+                OutlinedButton(
+                  onPressed: (){ 
+                    Navigator.pop(context);
+                  }, 
+                  child: const Text("Cancel") 
+                  )
+            ],
+          ),
         ],
       ),
     );
